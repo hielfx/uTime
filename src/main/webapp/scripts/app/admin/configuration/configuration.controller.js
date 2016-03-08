578b888318e492d118468bc55359d6202ff75984
Daniel Sánchez <dansanav@gmail.com>
2016-03-08 15:30:28 +0100

    Initial Commit

---
+++ src/main/webapp/scripts/app/admin/configuration/configuration.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('volunteercrowdApp')
+    .controller('ConfigurationController', function ($scope, ConfigurationService) {
+        ConfigurationService.get().then(function(configuration) {
+            $scope.configuration = configuration;
+        });
+    });
---
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('volunteercrowdApp')
+    .controller('MainController', function ($scope, Principal) {
+        Principal.identity().then(function(account) {
+            $scope.account = account;
+            $scope.isAuthenticated = Principal.isAuthenticated;
+        });
+    });
